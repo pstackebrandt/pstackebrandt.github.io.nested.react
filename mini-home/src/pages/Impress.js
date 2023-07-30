eab5a5d1f1bc65ba8e894bf7d8137a01ade163cb
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-30 19:16:28 +0200

    Impress: Format page

--- mini-home/src/pages/Impress.js
+++ mini-home/src/pages/Impress.js
@@ -1,27 +1,48 @@
+// file: Impress.js
+
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Impress() {
-
     return (
-        <Container className="d-flex justify-content-center">
-            <h1>Impress</h1>
-            <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
-            <p>Peter Stackebrandt<br />
-                Wei&szlig;ensee<br />
-                27<br />
-                90537 Feucht</p>
-
-            <h2>Kontakt</h2>
-            <p>E-Mail: info.stackebrandt@gmail.com</p>
-
-            <h2>Redaktionell verantwortlich</h2>
-            <p>Peter Stackebrandt</p>
-
-            <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
+        <Container>
+            <Row className="mb-4">
+                <Col>
+                    <h1>Impressum</h1>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+                    <address>
+                        Peter Stackebrandt<br />
+                        Wei&szlig;ensee 27<br />
+                        90537 Feucht
+                    </address>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <h2>Kontakt</h2>
+                    <p>E-Mail: <a href="mailto:info.stackebrandt@gmail.com">info.stackebrandt@gmail.com</a></p>
+                </Col>
+            </Row>
+            <Row className="mb-4">
+                <Col>
+                    <h2>Redaktionell verantwortlich</h2>
+                    <p>Peter Stackebrandt</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p>Quelle: <a href="https://www.e-recht24.de" target="_blank" rel="noopener noreferrer">e-recht24.de</a></p>
+                </Col>
+            </Row>
         </Container>
     );
 }
-
 
 export default Impress;
--- mini-home/src/pages/Welcome.js
+++ mini-home/src/pages/Welcome.js
@@ -1,8 +1,5 @@
 import React from "react"
 
-// styles
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import PageTitle from '../pages/page-title';
 
 // Main content of the page
