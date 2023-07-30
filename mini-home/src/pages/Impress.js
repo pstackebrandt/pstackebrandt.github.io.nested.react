fc288f064557de217ec13be8bf30cc9bd15f3c6f
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2023-07-30 19:01:09 +0200

    Impress: Add content

--- mini-home/src/pages/Impress.js
+++ mini-home/src/pages/Impress.js
@@ -6,6 +6,19 @@ function Impress() {
     return (
         <Container className="d-flex justify-content-center">
             <h1>Impress</h1>
+            <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+            <p>Peter Stackebrandt<br />
+                Wei&szlig;ensee<br />
+                27<br />
+                90537 Feucht</p>
+
+            <h2>Kontakt</h2>
+            <p>E-Mail: info.stackebrandt@gmail.com</p>
+
+            <h2>Redaktionell verantwortlich</h2>
+            <p>Peter Stackebrandt</p>
+
+            <p>Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a></p>
         </Container>
     );
 }
